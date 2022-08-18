d09a4c138d364e567dbe436abadaa7c919c76d88
m1ria <chzaaow@gmail.com>
2022-08-19 04:58:10 +0800

    Initial commit

---
+++ src/test/java/netty/c1/TestByteBuffer.java
@@ -0,0 +1,40 @@
+package netty.c1;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @className: TestByteBuffer
+ * @description: TODO
+ * @author: m1ria
+ * @date: 2022/8/19 4:11
+ * @version: 1.0
+ */
+@Slf4j
+public class TestByteBuffer {
+
+    public static void main(String[] args) {
+        try (FileChannel channel = new FileInputStream("data.txt").getChannel()) {
+            ByteBuffer buffer = ByteBuffer.allocate(10);
+            while (true) {
+                int len = channel.read(buffer);
+                log.debug("读取到的字节数 {}", len);
+                if (len == -1) {
+                    break;
+                }
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    byte b = buffer.get();
+                    log.debug("实际字节 {}", (char) b);
+                }
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
