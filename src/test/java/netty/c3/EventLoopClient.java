c4f517f7e2add3a7a3351e22c3d0a9a5c67d3c6a
m1ria <chzaaow@gmail.com>
2022-09-20 01:57:00 +0800

    eventloop

---
+++ src/test/java/netty/c3/EventLoopClient.java
@@ -0,0 +1,36 @@
+package netty.c3;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @className: HelloClient
+ * @description: TODO
+ * @author: m1ria
+ * @date: 2022/9/15 16:17
+ * @version: 1.0
+ */
+public class EventLoopClient {
+    public static void main(String[] args) throws InterruptedException {
+        Channel channel = new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost", 8002))
+                .sync()
+                .channel();
+        System.out.println(channel);
+        System.out.println("");
+    }
+}
